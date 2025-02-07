a47bf17dd261c6416ef1297d0dcaf643f9f0cb87
AdeleGropplero <adele.gdt@hotmail.com>
2025-02-07 16:36:57 +0100

    first

--- src/main/java/com/exam/springData1/Exam13/Repository/EdificioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.exam.springData1.Exam13.Repository;
-
-import com.exam.springData1.Exam13.Models.Edificio;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EdificioRepository extends JpaRepository<Edificio, Integer> {
-
-    boolean existsByNome(String nome);
-}
